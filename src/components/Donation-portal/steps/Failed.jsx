3989b713141593c36c369dedd05bee2225db6e9b
daniyal-p <daniyal733khan@gmail.com>
2025-01-09 19:18:50 +0530

    ui updated

--- src/components/Donation-portal/steps/Failed.jsx
+++ src/components/Donation-portal/steps/Failed.jsx
@@ -3,36 +3,20 @@ import "../../../index.css"
 import { Link } from "react-router-dom";
 
 const Failed = () => {
-  
-  useEffect(() => {
-    // Load Tenor Embed Script
-    const script = document.createElement("script");
-    script.src = "https://tenor.com/embed.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }, []);
-
+  const referenceId = Array(20)
+  .fill(0)
+  .map(() => "12345abcde"[Math.floor(Math.random() * 10)])
+  .join("");
   return (
     <div className="flex justify-center items-center">
       <div className="bg-white rounded-lg p-8 w-full max-w-md text-center">
         <h2 className="text-2xl font-bold mb-4">Transaction Failed</h2>
 
-        <div className="text-white rounded-full w-12 h-12 mx-auto mb-4 flex justify-center items-center">
-          <div
-            className="tenor-gif-embed"
-            data-postid="16261799211561498106"
-            data-share-method="host"
-            data-aspect-ratio="1"
-            data-width="100%"
-          >
-            <a href="https://tenor.com/view/camp-dayz-no-gif-16261799211561498106">
-              Camp Dayz No Sticker
-            </a>
-            from <a href="https://tenor.com/search/camp+dayz+no-stickers">Camp Dayz No Stickers</a>
-          </div>
+        <div className="flex justify-center items-center bg-red-500  w-20 h-20 rounded-full mx-auto mb-6">
+        <p className="text-[50px] text-white">X</p>
         </div>
 
-        <p className="text-gray-700 mb-6">Order #123RGR231567Y</p>
+        <p className="text-gray-700 mb-6">Order #SO-{referenceId}</p>
 
         <button className="bg-[#02343F] hover:bg-[#02343fd9] text-white font-bold py-2 px-4 rounded w-full">
           <Link to="/" className="text-white">
