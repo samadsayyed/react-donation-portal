e75bcf24429a5f77c7c4d9b91cf4e4bcb875597e
samadsayyed <samadsayyed812@gmail.com>
2025-01-13 18:00:38 +0530

    empty 2

--- src/components/Donation-portal/steps/DonationCart.jsx
+++ src/components/Donation-portal/steps/DonationCart.jsx
@@ -278,7 +278,7 @@ setRefetch(!refetch)
                     className="flex items-center mb-4"
                     key={`${item.cart_id}-${index}`}
                   >
-                    {/* <div className="flex items-center w-2/5 justify-center">
+                    <div className="flex items-center w-2/5 justify-center">
                       <span className="text-sm font-medium text-gray-600">
                         Donate For Yourself
                       </span>
@@ -293,7 +293,7 @@ setRefetch(!refetch)
                         />
                         <div className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-teal-600"></div>
                       </label>
-                    </div> */}
+                    </div>
 
                     <input
                       type="text"
